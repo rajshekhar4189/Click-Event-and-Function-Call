3017fd3d7a6318257349b82fddc00091c49d898c
Rajshekhar Choudhury <71339038+rajshekhar4189@users.noreply.github.com>
2022-01-29 19:30:52 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import Users from './Users.js';
+import './App.css';
+
+
+function App() {
+  return (
+         <div className="App">
+         <h1>Hello World!</h1>
+         <Users /> 
+       </div>
+       
+  );
+}
+
+export default App;
