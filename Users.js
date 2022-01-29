8adfdef86fbaaef0818cb96c794141333b2567be
Rajshekhar Choudhury <71339038+rajshekhar4189@users.noreply.github.com>
2022-01-29 19:30:20 +0530

    Create Users.js

---
+++ Users.js
@@ -0,0 +1,15 @@
+function Users()
+{
+    function raj()
+    {
+        alert("Some Error detected");
+    }
+    return(
+        <div>
+        <h1>Rajshekhar Choudhury</h1>
+        <button onClick={()=>raj()}>Click Me </button>
+        </div>
+        
+    );
+}
+export default Users;
